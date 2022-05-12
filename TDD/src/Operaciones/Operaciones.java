392e858718738ea56e7d57e6cf644ce86460e06e
santycarrillo17@gmail.com <santycarrillo17@gmail.com>
2022-05-12 15:25:07 -0500

    [GREEN]-[RES]-Metodo resta creado

--- TDD/src/Operaciones/Operaciones.java
+++ TDD/src/Operaciones/Operaciones.java
@@ -3,13 +3,24 @@ package Operaciones;
 public class Operaciones {
 
 	public void suma() {
-		
+
 		int a =5;
 		int b =8;
 		int res = a+b;
-		
+
 		System.out.println("El resultado de la suma es: "+ res);
-		
+
 	}
-	
+
+
+	public void resta() {
+
+		int a =5;
+		int b =8;
+		int res = a-b;
+
+		System.out.println("El resultado de la resta es: "+ res);
+
+	}
+
 }
