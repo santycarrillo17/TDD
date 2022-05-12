0c0066d2a934838f86a51da4acb259f986eb1c83
santycarrillo17@gmail.com <santycarrillo17@gmail.com>
2022-05-12 15:34:20 -0500

    [RED]-[DIV]-Metodo division no existe.

--- TDD/src/Test/DivisionTest.java
+++ TDD/src/Test/DivisionTest.java
@@ -4,11 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Operaciones.Operaciones;
+
 class DivisionTest {
 
 	@Test
 	void test() {
-		
+		Operaciones operaciones = new Operaciones();
+		operaciones.division();
 	}
 
 }
