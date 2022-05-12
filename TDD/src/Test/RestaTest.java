5751f18310cc17ea7885658db8131ed6d0e88246
santycarrillo17@gmail.com <santycarrillo17@gmail.com>
2022-05-12 15:22:53 -0500

    [RED]-[RES]-Metodo resta no existe.

--- TDD/src/Test/RestaTest.java
+++ TDD/src/Test/RestaTest.java
@@ -4,10 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Operaciones.Operaciones;
+
 class RestaTest {
 
 	@Test
 	void test() {
+		
+		Operaciones operaciones = new Operaciones();
+		operaciones.resta();
 		
 	}
 
