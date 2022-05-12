889877854ae8611b5c760c114bb92aa314bab160
santycarrillo17@gmail.com <santycarrillo17@gmail.com>
2022-05-12 15:16:19 -0500

    [RED]-[SUM]-Metodo Suma() no exite.

--- TDD/src/Test/SumaTest.java
+++ TDD/src/Test/SumaTest.java
@@ -1,13 +1,16 @@
 package Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import Operaciones.Operaciones;
 
 class SumaTest {
 
 	@Test
 	void test() {
+		
+		Operaciones operaciones = new Operaciones();
+		operaciones.suma();
 		
 	}
 
