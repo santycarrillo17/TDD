fb7bf92514f20b25e8d4b4d85d69ab702fe1bab1
santycarrillo17@gmail.com <santycarrillo17@gmail.com>
2022-05-12 15:19:53 -0500

    [GREEN]-[SUM]-Metodo suma creado.

--- TDD/src/Operaciones/Operaciones.java
+++ TDD/src/Operaciones/Operaciones.java
@@ -2,4 +2,14 @@ package Operaciones;
 
 public class Operaciones {
 
+	public void suma() {
+		
+		int a =5;
+		int b =8;
+		int res = a+b;
+		
+		System.out.println("El resultado de la suma es: "+ res);
+		
+	}
+	
 }
